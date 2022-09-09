3c99a4b4abeb60518d74571358956dd550c9e186
PylaevDmitry <pilaevdmitry@yandex.ru>
2022-09-09 14:52:35 +0400

    to h2

--- src/main/java/ru/pylaev/toDoProject/ToDoMain.java
+++ src/main/java/ru/pylaev/toDoProject/ToDoMain.java
@@ -3,7 +3,11 @@ package ru.pylaev.toDoProject;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import ru.pylaev.toDoProject.presentLayer.runnableUI.console.ConsoleUI;
+import ru.pylaev.toDoProject.presentLayer.runnableUI.window.WindowUi;
 import ru.pylaev.util.CustomProperties;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class ToDoMain {
@@ -12,10 +16,10 @@ public class ToDoMain {
     public static void main (String[] args){
         ApplicationContext context = new SpringApplicationBuilder(ToDoMain.class).headless(false).run(args);
 
-//        var executorService = Executors.newCachedThreadPool();
-//
-//        executorService.execute(context.getBean(ConsoleUI.class));
-//        executorService.execute(context.getBean(WindowUi.class));
+        var executorService = Executors.newCachedThreadPool();
+
+        executorService.execute(context.getBean(ConsoleUI.class));
+        executorService.execute(context.getBean(WindowUi.class));
 //        executorService.execute(context.getBean(TelegramUi.class));
 
     }
