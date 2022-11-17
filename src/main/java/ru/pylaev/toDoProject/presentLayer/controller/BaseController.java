035f59e0557a58ca80f9810dfcd90be94ce0d8b7
PylaevDmitry <pilaevdmitry@yandex.ru>
2022-11-17 13:28:29 +0400

    fix file bug

---
+++ src/main/java/ru/pylaev/toDoProject/presentLayer/controller/BaseController.java
@@ -0,0 +1,31 @@
+package ru.pylaev.toDoProject.presentLayer.controller;
+
+import ru.pylaev.toDoProject.businessLogicLayer.UiStateModel;
+import ru.pylaev.toDoProject.dataAccessLayer.Task;
+import ru.pylaev.toDoProject.abstractions.Observer;
+import ru.pylaev.toDoProject.presentLayer.factories.BaseUiFactory;
+import ru.pylaev.toDoProject.presentLayer.view.UniversalViewHandler;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public abstract class BaseController implements Observer {
+    protected UniversalViewHandler viewHandler;
+    protected UiStateModel uiStateModel;
+    protected BiConsumer<String, UiStateModel> respondLogic;
+    protected BiConsumer<String, List<Task>> graphLogic;
+
+    public BaseController(BaseUiFactory factory) {
+        this.viewHandler = factory.getViewHandler();
+        this.uiStateModel = factory.getUiStateModel();
+        this.respondLogic = factory.getRespondLogic();
+        this.graphLogic = factory.getGraphLogic();
+        uiStateModel.addObserver(viewHandler);
+        uiStateModel.addObserver(this);
+    }
+
+    @Override
+    public void update(String message, List<Task> tasks) {
+        graphLogic.accept(message, tasks);
+    }
+}
